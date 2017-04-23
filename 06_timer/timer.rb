5ba3003e4d4b081aec345c63b2283eda774c9250
Klaas <klaaseebes@gmail.com>
2017-04-23 19:54:06 +0200

    added timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,34 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def padded(digit)
+    if(digit < 10)
+      digit = "0#{digit}"
+    end
+    digit.to_s
+  end
+
+  def time_string
+    minutes = 0
+    hours = 0
+    seconds = self.seconds
+    if seconds > 60
+      remaining_seconds = seconds % 60
+      minutes = ( seconds - remaining_seconds ) / 60
+      seconds = remaining_seconds
+    end
+
+    if minutes > 60
+      remaining_minutes = minutes % 60
+      hours = (minutes - remaining_minutes) / 60
+      minutes = remaining_minutes
+    end
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
 end
